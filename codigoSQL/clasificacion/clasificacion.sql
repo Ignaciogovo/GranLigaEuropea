037f5dd4dfb9b1cd9a5fc999f07cd2b543b021e2
pollitodetroya <ignaciogovo@gmail.com>
2022-01-04 21:37:48 +0100

    Añadido datos a clasificación y su modificación para añadir columnas goles a favor y contra

---
+++ codigoSQL/clasificacion/clasificacion.sql
@@ -0,0 +1,22 @@
+
+
+INSERT INTO [clasificacion] (id_club)
+VALUES
+  (1),
+  (2),
+  (3),
+  (4),
+  (5),
+  (6),
+  (7),
+  (8),
+  (9),
+  (10);
+INSERT INTO [clasificacion] (id_club)
+VALUES
+  (11),
+  (12),
+  (13),
+  (14),
+  (15),
+  (16);
