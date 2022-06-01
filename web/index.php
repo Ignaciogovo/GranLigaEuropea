02e5a69bd9240f5eb7717c8af0b064a4a78cd269
pollitodetroya <ignaciogovo@gmail.com>
2022-06-02 01:47:36 +0200

    web

---
+++ web/index.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GranLigaEuropea</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;600&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<?php
+?>
+<body>
+    <section class="mb-5">
+        <header class="bg-dark text-light w-100 p-2">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <!-- <a class="navbar-brand" href="#">Navbar w/ text</a> -->
+                    <span class="navbar-text ms-5">
+                        <h1>Gran liga europea</h1>
+                    </span>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav ms-auto me-4 mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="active link-warning" aria-current="page" href="#">Calendario</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="m-3 link-warning" href="#">Clasificación</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="link-warning" href="#">Estadisticas</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="row">
+                <div class="col-6">
+                </div>
+            </div>
+        </header>
+        <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <table class="table table-striped mb-t">
+                    <thead>
+                        <tr>
+                            <th scope="col">Puesto</th>
+                            <th scope="col">CLUB</th>
+                            <th scope="col">puntos</th>
+                            <th scope="col">golesAfavor</th>
+                            <th scope="col">GolesEncontra</th>
+                            <th scope="col">golaveraje</th>
+                            <th scope="col">golaveraje</th>
+                            <th scope="col">golaveraje</th>
+                            <th scope="col">golaveraje</th>
+                            <th scope="col">golaveraje</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="col">1</th>
+                            <td>Madrid</td>
+                            <td>3</td>
+                            <td>10</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">2</th>
+                            <td>Barcelona</td>
+                            <td>7</td>
+                            <td>14</td>
+                            <td>12</td>    
+                            <td>5</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">3</th>
+                            <td> At Madrid</td>
+                            <td>3</td>
+                            <td>13</td>
+                            <td>12</td>    
+                            <td>15</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>Manchester</td>
+                            <td>38</td>
+                            <td>45</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>PSG</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">3</th>
+                            <td> At Madrid</td>
+                            <td>3</td>
+                            <td>13</td>
+                            <td>12</td>    
+                            <td>15</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>Manchester</td>
+                            <td>38</td>
+                            <td>45</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>PSG</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">1</th>
+                            <td>Madrid</td>
+                            <td>3</td>
+                            <td>10</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">2</th>
+                            <td>Barcelona</td>
+                            <td>7</td>
+                            <td>14</td>
+                            <td>12</td>    
+                            <td>5</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">3</th>
+                            <td> At Madrid</td>
+                            <td>3</td>
+                            <td>13</td>
+                            <td>12</td>    
+                            <td>15</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>Manchester</td>
+                            <td>38</td>
+                            <td>45</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>PSG</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">3</th>
+                            <td> At Madrid</td>
+                            <td>3</td>
+                            <td>13</td>
+                            <td>12</td>    
+                            <td>15</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>Manchester</td>
+                            <td>38</td>
+                            <td>45</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        <tr>
+                            <th scope="col">4</th>
+                            <td>PSG</td>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>12</td>    
+                            <td>12</td>    
+                        </tr>
+                        </tbody>
+                </table>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+    </section>
+    <!-- footer -->
+    <footer class="text-center bg-light mt-auto">
+        <div class="row pt-5 g-0">
+            <div class="col-12">
+                <div class="row g-0">
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="col-12 pb-4 text-dark">
+                                <h6>Enlaces</h6>
+                            </div>
+                            <div class="col-12 col-sm-4 m-auto">
+                                <a href="https://github.com/Ignaciogovo" class="link-warning">github</a>
+                            </div>
+                            <div class="col-12 col-sm-4 m-auto">
+                                <a href="https://twitter.com/granligaeuropa" class="link-warning">twitter</a>
+                            </div>
+                            <div class="col-12 col-sm-4 m-auto">
+                                <a href="https://po.ta.to/" class="link-warning">linkedln</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mt-5 bg-secondary">
+                <div class="text-light">© 2022 Copyright:
+                    <a class="link-warning" href="https://github.com/Ignaciogovo">Ignaciogovo</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
