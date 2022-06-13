7805aec9dc2f53d162f7b7bf6b314e1fbd22ae33
pollitodetroya <ignaciogovo@gmail.com>
2022-06-13 16:10:53 +0200

    Configuración del archivo de backup

--- CreacionBBDDYEstadisticas/backup/BackupEjecutar.sh
+++ CreacionBBDDYEstadisticas/backup/BackupEjecutar.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+# Hay que cambiar las rutas
 #  Realizamos backup
 archivo=$(cat jornadaTemporada.txt)
-mysqldump --defaults-extra-file=/data/database/my.cnf  liga> /ruta/$archivo
+mysqldump --defaults-extra-file=/ruta/datos/autenticación  liga> /ruta/$archivo
 cp /ruta/$archivo /ruta2/$archivo # Copiamos la copia de seguridad en otro disco
 
 # Necesitamos tener una conexion ssh con otro servidor para el siguiente comando
 #  introducimos la clave al agente ssh
 eval `ssh-agent`
-ssh-add /home/web/.ssh/oracle/id_rsa
+ssh-add /ruta/id_rsa
 # copiamos el archivo creado y hacemos un restore en el otro servidor de escucha
 scp /ruta/$archivo usuario@IP:/ruta/backup.sql
 ssh usuario@IP "sudo mysql liga < /ruta/backup.sql"
