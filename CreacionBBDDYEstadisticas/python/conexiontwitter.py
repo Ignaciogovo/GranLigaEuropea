fb6b0f1b69111dcbab3f605ed15de296385f2a3c
ignaciogovo <ignaciogovo@gmail.com>
2023-07-26 19:10:48 +0200

    Evitar que salten errores a la hora de realizar los tweet que perjudiquen a la ejecución de la jornada

--- CreacionBBDDYEstadisticas/python/conexiontwitter.py
+++ CreacionBBDDYEstadisticas/python/conexiontwitter.py
@@ -16,33 +16,40 @@ control_twitter=os.getenv('CONTROL_TWITTER')
 def twittearJornada(jornada):
     # Comprobamos si se quiere twitterar
     if control_twitter == 'S':
-        auth=cp.tokensTwitter()
-        api = tweepy.API(auth)
-        # Publicar tweet
-        texto = "Inicio de la Jornada: "+str(jornada)
-        if jornada==1:
-            texto = "Comienza la temporada con el inicio de la jornada: "+str(jornada)
-        api.update_status(texto)
-
+        try:
+            auth=cp.tokensTwitter()
+            api = tweepy.API(auth)
+            # Publicar tweet
+            texto = "Inicio de la Jornada: "+str(jornada)
+            if jornada==1:
+                texto = "Comienza la temporada con el inicio de la jornada: "+str(jornada)
+            api.update_status(texto)
+        except:
+            print("Error al realizar el tweet inicio jornada")
 def twittearPartido(local,visitante,gol_local,gol_visitante):
     # Comprobamos si se quiere twitterar
     if control_twitter == 'S':
-        auth=cp.tokensTwitter()
-        local = cs.selectNombreClub(local)
-        visitante = cs.selectNombreClub(visitante)
-        api = tweepy.API(auth)
-        texto = local+" "+str(gol_local )+" VS "+visitante+" "+str(gol_visitante)
-        api.update_status(texto)
-
+        try:
+            auth=cp.tokensTwitter()
+            local = cs.selectNombreClub(local)
+            visitante = cs.selectNombreClub(visitante)
+            api = tweepy.API(auth)
+            texto = local+" "+str(gol_local )+" VS "+visitante+" "+str(gol_visitante)
+            api.update_status(texto)
+        except:
+            print("Error al realizar el tweet partido: "+str(local)+" "+str(gol_local )+" VS "+str(visitante)+" "+str(gol_visitante))
 def twittearFinal():
     # Comprobamos si se quiere twitterar
     if control_twitter == 'S':
-        auth=cp.tokensTwitter()
-        campeon = cs.selectCampeonLiga()
-        temporada = str(cs.selectTemporada())
-        api = tweepy.API(auth)
-        texto =  "¡¡FINAAAL DE LA TEMPORADA: "+ temporada + "!! Felicidades a " + campeon +" por su campeonato ligero."
-        api.update_status(texto)
+        try:
+            auth=cp.tokensTwitter()
+            campeon = cs.selectCampeonLiga()
+            temporada = str(cs.selectTemporada())
+            api = tweepy.API(auth)
+            texto =  "¡¡FINAAAL DE LA TEMPORADA: "+ temporada + "!! Felicidades a " + campeon +" por su campeonato ligero."
+            api.update_status(texto)
+        except:
+            print("Error al realizar tweet final temporada")
 
 
 
@@ -52,6 +59,9 @@ def twittearFinal():
 def twittearMensaje(mensaje):
     # Comprobamos si se quiere twitterar
     if control_twitter == 'S':
-        auth=cp.tokensTwitter()
-        api = tweepy.API(auth)
-        api.update_status(mensaje)
+        try:
+            auth=cp.tokensTwitter()
+            api = tweepy.API(auth)
+            api.update_status(mensaje)
+        except:
+            print("error al realizar insert mensaje")
